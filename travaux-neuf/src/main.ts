153e2e05db5e2181f9c88fc60b7948c4a15a70f2
Alan Jacob <55379884+AlanJacobdev@users.noreply.github.com>
2022-07-06 17:12:17 +0200

    origin

--- travaux-neuf/src/main.ts
+++ travaux-neuf/src/main.ts
@@ -6,7 +6,7 @@ import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    "origin": "*"
+    "origin": true
   });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
