9d12ffe5b2a24912bc44f86d1c2dcc665f25714c
Alan Jacob <55379884+AlanJacobdev@users.noreply.github.com>
2022-07-20 15:04:06 +0200

    Disable logger, keep warn & error

--- travaux-neuf/src/main.ts
+++ travaux-neuf/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
   app.enableCors({
     "origin": true
   });
